dbea92d2fa2a076e443bc5be750e3b6176885b87
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2019-01-28 16:40:03 +0900

    2주차
    Signed-off-by: KyeongSoo Yoo <dev.yoogomja@gmail.com>

--- week2/scripts/practice2.solution.js
+++
@@ -1,7 +0,0 @@
-function select(obj){
-    $('.nav-link').removeClass("active");
-    $(obj).find('.nav-link').addClass("active");
-    var currentEq = $(obj).attr("data-nav-eq");
-    $('.jumbotron').removeClass("hide").addClass("hide");
-    $(".jumbotron[data-box-eq=" + currentEq+ ']').removeClass("hide");
-}
