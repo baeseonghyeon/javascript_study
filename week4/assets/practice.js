9e238245015ee17729a359d08139193d722efcf5
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2019-02-18 19:09:29 +0900

    practice 1차완성
    Signed-off-by: KyeongSoo Yoo <dev.yoogomja@gmail.com>

--- week4/assets/practice.js
+++ week4/assets/practice.js
@@ -1,88 +1,39 @@
 var _BOARD = _BOARD || {
     config : {
         url : 'http://syunoti.cafe24app.com/api/boards/get',
-        method : 'GET'
+        method : 'GET',
+        connectState : false
     },
     methods : {
         ui : {
             init : function(){
-                $('#optCate').val('');
-                $('#optType').val('');
-                $('#txtKeyword').val('');
+                // UI를 초기화하고 값을 할당해줄 함수입니다.
             },
             search : function(){
-                _BOARD.methods.data.init({
-                    cate : $('#optCate').val(),
-                    type : $('#optType').val(),
-                    keyword : $('#txtKeyword').val()
-                })
-                _BOARD.methods._data.load({},function(res){
-                    _BOARD.methods.board.init(res.data.items);
-                    _BOARD.methods.page.init(res.data.paging);
-                });
-            }
-        },
-        page : {
-            init : function(data){
-                console.log(data);
+                // 검색 버튼을 눌렀을 떄 실행될 함수입니다.
             },
-            go : function(){
-
+            reset : function(){
+                // 초기화 버튼을 눌렀을 때 실행될 함수입니다.
             }
         },
         board : {
             init : function(data){
-                var _mockup = $('.mockup').find('tr');
-                var _tbody = $('#tblBoard').find('tbody');
-                if(_tbody.children().length > 0){
-                    _tbody.children().remove();
-                }
-                for(var i = 0 ; i < data.length ; i++){
-                    var item = data[i];
-                    var row = _mockup.clone();
-                    if(!item.isNotice){
-                        row.find('.seq').text(item.seq);
-                    }
-                    else {
-                        row.find('.seq').text('공지');
-                    }
-
-                    row.find('.cate').text(item.cate);
-                    row.find('.author').text(item.author);
-                    row.find('.date').text(item.date);
-                    row.find('.title').find('.link').prop('href' , item.location);
-                    row.find('.title').find('.link').text(item.title);
-                    _tbody.append(row);
-                }
-            }
+                // 게시판 내용을 채울 함수입니다.
+            }      
         },
         _data : {
             load : function(options , callback){
-                $.ajax({
-                    url : _BOARD.config.url,
-                    method : _BOARD.config.method
-                })
-                .then(function(res){
-                    callback({
-                        status : res.code,
-                        data : res.data
-                    });
-                } , function(data){
-                    callback({
-                        status : 'ERROR',
-                        data : {}
-                    });
-                });
+                // 데이터를 가져오는 함수입니다. 
+                // jquery ajax함수는 여기서 사용합니다.
             }
         }
     }
 }
 
 $(function(){
-    console.log(_BOARD);
-    _BOARD.methods._data.load({},function(res){
-        _BOARD.methods.ui.init();
-        _BOARD.methods.board.init(res.data.items);
-        _BOARD.methods.page.init(res.data.paging);
-    });
+    // _BOARD.methods._data.load({},function(res){
+    //     _BOARD.methods.ui.init();
+    //     _BOARD.methods.board.init(res.data.items);
+    //     _BOARD.methods.page.init(res.data.paging);
+    // });
 });
---
+++ week4/assets/practice.solution.js
@@ -0,0 +1,103 @@
+var _BOARD = _BOARD || {
+    config : {
+        url : 'http://syunoti.cafe24app.com/api/boards/get',
+        method : 'GET',
+        connectState : false
+    },
+    methods : {
+        ui : {
+            init : function(){
+                $('#optCate').val('');
+                $('#optType').val('');
+                $('#txtKeyword').val('');
+            },
+            search : function(){
+                
+                _BOARD.methods._data.load({
+                    cate : $('#optCate').val(),
+                    type : $('#optType').val(),
+                    keyword : $('#txtKeyword').val()
+                },function(res){
+                    console.log(res);
+                    _BOARD.methods.board.init(res.data.items);
+                    _BOARD.methods.page.init(res.data.paging);
+                });
+            },
+            reset : function(){
+                _BOARD.methods.ui.init();
+                _BOARD.methods._data.load({},function(res){
+                    _BOARD.methods.board.init(res.data.items);
+                    _BOARD.methods.page.init(res.data.paging);
+                });
+            }
+        },
+        page : {
+            init : function(data){
+                console.log(data);
+            },
+            go : function(){
+
+            }
+        },
+        board : {
+            init : function(data){
+                var _mockup = $('.mockup').find('tr');
+                var _tbody = $('#tblBoard').find('tbody');
+                if(_tbody.children().length > 0){
+                    _tbody.children().remove();
+                }
+                for(var i = 0 ; i < data.length ; i++){
+                    var item = data[i];
+                    var row = _mockup.clone();
+                    if(!item.isNotice){
+                        row.find('.seq').text(item.seq);
+                    }
+                    else {
+                        row.find('.seq').text('공지');
+                    }
+
+                    row.find('.cate').text(item.cate);
+                    row.find('.author').text(item.author);
+                    row.find('.date').text(item.date);
+                    row.find('.title').find('.link').prop('href' , item.location);
+                    row.find('.title').find('.link').text(item.title);
+                    _tbody.append(row);
+                }
+            }
+        },
+        _data : {
+            load : function(options , callback){
+                if(_BOARD.config.connectState){alert('불러오는 중입니다. 잠시만 기다려주세요.')}
+                else{
+                    _BOARD.config.connectState = true;
+                    $.ajax({
+                        url : _BOARD.config.url,
+                        method : _BOARD.config.method,
+                        data : options
+                    })
+                    .then(function(res){
+                        _BOARD.config.connectState = false;
+                        callback({
+                            status : res.code,
+                            data : res.data
+                        });
+                    } , function(data){
+                        callback({
+                            status : 'ERROR',
+                            data : {}
+                        });
+                    });
+                }
+                
+            }
+        }
+    }
+}
+
+$(function(){
+    _BOARD.methods._data.load({},function(res){
+        _BOARD.methods.ui.init();
+        _BOARD.methods.board.init(res.data.items);
+        _BOARD.methods.page.init(res.data.paging);
+    });
+});
